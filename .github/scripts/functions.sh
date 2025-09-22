9ce2caa50e9ad3f7a31e96cdee2793d6c8720079
Daniel Manners <daniel.a.manners@gmail.com>
2025-09-22 11:49:34 -0400

    feat: Adding a KWOK Action workflow to validate things (#8)
    fix: Updating the KWOK validation workflow
    
    Signed-off-by: Dan Manners <daniel.a.manners@gmail.com>

---
+++ .github/scripts/functions.sh
@@ -0,0 +1,7 @@
+# Scrape all namespaces from kubernetes YAML files
+function get_namespaces() {
+	for file in $(find kubernetes/ -name '*.yaml' -type f); do
+		cat ${file} | yq e 'explode(.)' |
+		grep 'namespace:' | awk '{print $2}'
+	done | tr -d '"' | sort | uniq | grep -Ev 'namespace|default|kube-' | xargs
+}
